f89796dab7bc3b91ff780122d743acc500c3e72b
sidabalok123 <144589967+sidabalok123@users.noreply.github.com>
2023-09-10 22:10:35 +0700

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,28 @@
+# latihan tugas konversi suhu 
+
+# program konversi suhu celcius kedalam fahrenheit 
+
+celcius= float(input('masukkan suhu kedalam celcius :'))
+print ("suhu adalah", celcius, "celcius")
+
+# fahrenheit
+fahrenheit =((9/5)*celcius)+32
+print ("suhu dalam fahrenheit adalah", fahrenheit, "fahrenheit")
+
+#program konversi suhu fahrenheit ke dalam celcius 
+
+fahrenheit = float(input('masukkan suhu kedalam fahrenheit :'))
+print ("suhu adalah", fahrenheit, "fahrenheit")
+
+# celcius
+celcius = ((5/9)*fahrenheit)-32
+print ("suhu dalam celcius adalah", celcius, "celcius")
+
+# program konversi suhu kelvin kedalam celcius 
+
+kelvin = float(input('masukkan suhu kedalam kelvin :'))
+print ("suhu adalah", kelvin, "kelvin")
+
+# celcius
+celcius = kelvin-273
+print ("suhu dalam celcius adalah", celcius, "celcius")
